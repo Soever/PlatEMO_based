401a7561534e3f941971325f08217421a515fc7b
LaptopSoever <LaptopSoever@Sobook>
2024-02-27 13:36:45 +0800

    test

--- SO/SnakeOptimizer.m
+++ SO/SnakeOptimizer.m
@@ -1,6 +1,7 @@
 classdef SnakeOptimizer < ALGORITHM
     methods
         function main(Algorithm,Problem)
+            global result_t ;
             %% Parameter setting
             N = Problem.N;  % Population size
             Nm=round(N/2);
@@ -30,6 +31,7 @@ classdef SnakeOptimizer < ALGORITHM
             [fitnessBest_f, gbest2] = min(fitness_f);
             Xbest_f = Xf(gbest2);
             t=0 ;
+            result_t(1)=GYbest;
             while Algorithm.NotTerminated(X)
             %   
                 t = t+1 ;
@@ -40,81 +42,19 @@ classdef SnakeOptimizer < ALGORITHM
        
                 if Q>1        Q=1;    end
                 if Q<Threshold
-                    for i=1:Nm
-                        for j=1:1:dim
-                            rand_leader_index = floor(Nm*rand()+1);
-                            X_randm = Xm(rand_leader_index);
-                            flag_index = floor(2*rand()+1);
-                            Flag=vec_flag(flag_index); 
-                            Am=exp(-fitness_m(rand_leader_index)/(fitness_m(i)+eps));%eq.(7)
-                            Xnewm(i,j)=X_randm.dec(j)+Flag*C2*Am*((ub(j)-lb(j)*rand+lb(j)));%eq.(6)
-                        end
-                    end
-                    for i=1:Nf
-                        for j=1:1:dim
-                            rand_leader_index = floor(Nf*rand()+1);
-                            X_randf = Xf(rand_leader_index);
-                            flag_index = floor(2*rand()+1);
-                            Flag=vec_flag(flag_index);
-                            Af=exp(-fitness_f(rand_leader_index)/(fitness_f(i)+eps));%eq.(9)
-                            Xnewf(i,j)=X_randf.dec(j)+Flag*C2*Af*((ub(j)-lb(j))*rand+lb(j));%eq.(8)
-                        end
-                    end
+                    %雄性探索
+                    Xnewm = so_exploration(Nm,dim,Xm,fitness_m,C2,Xnewm,ub,lb);
+                    Xnewf = so_exploration(Nf,dim,Xf,fitness_m,C2,Xnewf,ub,lb);
                 else %Exploitation Phase (Food Exists)
                     if Temp>Thresold2  %hot
-                        for i=1:Nm
-                            flag_index = floor(2*rand()+1);
-                            Flag=vec_flag(flag_index);
-                            for j=1:1:dim
-                                Xnewm(i,j)=Xfood.dec(j)+C3*Flag*Temp*rand*(Xfood.dec(j)-Xnewm(i,j));%eq.(10)
-                            end
-                        end
-                        for i=1:Nf
-                            flag_index = floor(2*rand()+1);
-                            Flag=vec_flag(flag_index);
-                            for j=1:1:dim
-                                Xnewf(i,j)=Xfood.dec(j)+Flag*C3*Temp*rand*(Xfood.dec(j)-Xnewf(i,j));%eq.(10)
-                            end
-                        end
+                        Xnewm =so_exploitation_food_hot(Nm,dim,Xfood,C3,Temp,Xnewm);
+                        Xnewf =so_exploitation_food_hot(Nf,dim,Xfood,C3,Temp,Xnewf);
                     else %cold
-                        if rand>0.6 %fight
-                            for i=1:Nm
-                                for j=1:1:dim
-                                    FM=exp(-(fitnessBest_f)/(fitness_m(i)+eps));%eq.(13)
-                                    Xnewm(i,j)=Xnewm(i,j) +C3*FM*rand*(Q*Xbest_f.dec(j)-Xnewm(i,j));%eq.(11)
-                                    
-                                end
-                            end
-                            for i=1:Nf
-                                for j=1:1:dim
-                                    FF=exp(-(fitnessBest_m)/(fitness_f(i)+eps));%eq.(14)
-                                    Xnewf(i,j)=Xnewf(i,j)+C3*FF*rand*(Q*Xbest_m.dec(j)-Xnewf(i,j));%eq.(12)
-                                end
-                            end
-                        else%mating
-                            for i=1:Nm
-                                for j=1:1:dim
-                                    Mm=exp(-fitness_f(i)/(fitness_m(i)+eps));%eq.(17)
-                                    Xnewm(i,j)=Xnewm(i,j) +C3*rand*Mm*(Q*Xnewf(i,j)-Xnewm(i,j));%eq.(15
-                                end
-                            end
-                            for i=1:Nf
-                                for j=1:1:dim
-                                    Mf=exp(-fitness_m(i)/(fitness_f(i)+eps));%eq.(18)
-                                    Xnewf(i,j)=Xnewf(i,j) +C3*rand*Mf*(Q*Xnewm(i,j)-Xnewf(i,j));%eq.(16)
-                                end
-                            end
-                            flag_index = floor(2*rand()+1);
-                            egg=vec_flag(flag_index);
-                            if egg==1;
-                                [GYworst, gworst] = max(fitness_m);
-                                Xnewm(gworst,:)=lb+rand*(ub-lb);%eq.(19)
-                                [GYworst, gworst] = max(fitness_f);
-                                Xnewf(gworst,:)=lb+rand*(ub-lb);%eq.(20)
-                            end
-                        end
+                       [Xnewm,Xnewf] = so_exploitation_food_cold(Nm,Nf,dim,fitnessBest_m,fitnessBest_f,fitness_m,fitness_f,C3,Q,Xbest_m,Xbest_f,Xnewm,Xnewf,lb,ub);
                     end
                 end
+
+                %更新雄性种群
                 for j=1:Nm
                     Xnewm = Problem.CalDec(Xnewm) ;
                     Xnewm_ind = Problem.Evaluation(Xnewm(j,:));
@@ -124,11 +64,9 @@ classdef SnakeOptimizer < ALGORITHM
                         Xm(j)= Xnewm_ind;
                     end
                 end
-                
                 [Ybest1,gbest1] = min(fitness_m);
-                
+                %更新雌性种群
                 for j=1:Nf
-
                     Xnewf = Problem.CalDec(Xnewf) ;
                     Xnewf_ind =Problem.Evaluation(Xnewf(j,:)) ;
                     y = Xnewf_ind.obj ;
@@ -137,10 +75,11 @@ classdef SnakeOptimizer < ALGORITHM
                         Xf(j)= Xnewf_ind;
                     end
                 end
-                X(1:Nm) = Xf ;
-                X(Nm+1:end) = Xf ;
                 [Ybest2,gbest2] = min(fitness_f);
                 
+                X(1:Nm) = Xm ;
+                X(Nm+1:end) = Xf ;
+                %更新雄性、雌性最优
                 if Ybest1<fitnessBest_m
                     Xbest_m = Xm(gbest1);
                     fitnessBest_m=Ybest1;
@@ -148,12 +87,15 @@ classdef SnakeOptimizer < ALGORITHM
                 if Ybest2<fitnessBest_f
                     Xbest_f = Xf(gbest2);
                     fitnessBest_f=Ybest2;
-                    
                 end
+                %记录全局最优
                 if Ybest1<Ybest2
                     gbest_t(t)=min(Ybest1);
                 else
                     gbest_t(t)=min(Ybest2);
+                end
+                if t == T-1
+                    result_t(2:t+1) = gbest_t ;
                 end
                 if fitnessBest_m<fitnessBest_f
                     GYbest=fitnessBest_m;
@@ -164,6 +106,8 @@ classdef SnakeOptimizer < ALGORITHM
                 end
 
             end
+            
+
         end
     end
 end
--- test.m
+++ test.m
@@ -1,8 +1,55 @@
-maxIteration = 500 ;
-N = 30 ;
-D = 30 ;
-maxFE = maxIteration*N ;
-platemo('problem',@CEC2017_F4,'algorithm',@SnakeOptimizer,'N',N,'D',D,'maxFE',15000);
-platemo('problem',@CEC2017_F4,'algorithm',@GA,'N',N,'D',D,'maxFE',15000);
-platemo('problem',@CEC2017_F4,'algorithm',@PSO,'N',N,'D',D,'maxFE',15000);
-platemo('problem',@CEC2017_F4,'algorithm',@SHADE,'N',N,'D',D,'maxFE',15000);
+clc
+clear
+
+
+
+N = 30 ;D = 30 ;
+maxIteration = 500 ;runs = 3 ;n_func = 2 ;
+al_handles = {@SnakeOptimizer,@GA,@DE,@PSO};
+maxFE = maxIteration*N ; 
+for i = 1:n_func
+    func_name = "CEC2017_F"+ num2str(i); % 创建函数名字符串
+    func_handles{i} = str2func(func_name); % 创建函数句柄
+end
+obj_min= zeros(length(al_handles),n_func,runs);
+best = zeros(length(al_handles),n_func,runs);
+global result_t ;
+for i =1:length(al_handles)
+    for j = 1:n_func
+        for k = 1:runs
+            al =al_handles{i} ;
+            func = func_handles{j} ;
+            [~,obj,~] = platemo('problem',func,'algorithm',al,'N',N,'D',D,'maxFE',maxFE);
+            if i == 1 %SO通过全局变量result_t记录
+                best(i,j,k) = result_t(end);
+            end
+            obj_min(i,j,k) = min(obj);
+            
+            % f = figure;
+            % plot(result_t,'LineWidth', 2, 'Color', 'k');
+            % title(strrep(char(al),'@','')+" on "+strrep(char(func),'@',''));
+        end
+    end
+end
+
+
+
+%platemo('problem',@CEC2017_F4,'algorithm',@GA,'N',N,'D',D,'maxFE',15000);
+%platemo('problem',@CEC2017_F4,'algorithm',@SnakeOptimizer,'N',N,'D',D,'maxFE',15000);
+
+
+
+
+% 
+% platemo('problem',@CEC2017_F4,'algorithm',@PSO,'N',N,'D',D,'maxFE',15000);
+% platemo('problem',@CEC2017_F4,'algorithm',@SHADE,'N',N,'D',D,'maxFE',15000);
+
+% runs = 5 ;
+% Obj_min = zeros(runs,1) ;
+% for i = 1:runs
+%     [~,Obj,~] = platemo('problem',@CEC2017_F8,'algorithm',@GA,'N',N,'D',D,'maxFE',15000);
+%     Obj_min = min(Obj);
+% end
+% display(mean(Obj_min));
+
+
