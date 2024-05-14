a94f0ac23b5d9dab1da9627ab9530c392d78efdf
Soputer <983026211@qq.com>
2024-05-14 19:08:50 +0800

    CEC test

--- CECFunctions/cec2021/cec2021_0512.m
+++ CECFunctions/cec2021/cec2021_0512.m
@@ -3,16 +3,18 @@ clc
 
 runs = 30 ;
 T = 500 ;
-N = 100 ;
+N = 50 ;
 dim =20 ;
 function_num = 10 ;
-result = zeros(function_num,3) ;
+
 fval1 = zeros(runs,function_num) ;
 fval2 = zeros(runs,function_num) ;
 fval3 = zeros(runs,function_num) ;
 fval4 = zeros(runs,function_num) ;
+result = zeros(function_num,4) ;
 for i=1:function_num
     Function_name=i; 
+
     [lb,ub,dim,fobj] = Get_Functions_cec2021(Function_name,dim);
     lb = lb(1);
     ub = ub(1);
@@ -27,9 +29,8 @@ for i=1:function_num
     end
     
 end
-result(i,1)=mean(fval1,1);
-result(i,2)=mean(fval2,1);
-result(i,3)=mean(fval3,1);
-result(i,4)=mean(fval4,1);
-
+result(:,1)=mean(fval1,1);
+result(:,2)=mean(fval2,1);
+result(:,3)=mean(fval3,1);
+result(:,4)=mean(fval4,1);
 disp(result)
